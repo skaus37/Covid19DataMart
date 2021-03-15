1977d9474a2368e9d429c329335a4d806932561d
Maxime <mcote074@uottawa.ca>
2021-03-15 18:12:28 -0400

    Update DICE_QUERIES.sql

--- OLAP/DICE_QUERIES.sql
+++ OLAP/DICE_QUERIES.sql
@@ -1,7 +1,7 @@
  --number of fatal cases for November and December in Peel(Mississauga) and Ottawa
 select L.phu_name, D.month, count(*) as "Fatal Cases" 
-from data_mart.fact_table F, data_mart.patient_dimension P, data_mart.reported_date_dimension D, data_mart.phu_location_dimension L 
-where F.reported_date_key=D.reported_date_key and F.patient_key=P.patient_key and F.phu_location_key=L.phu_location_key and F.is_fatal=true and D.month in ('November','December') and L.city in ('Mississauga','Ottawa') 
+from data_mart.fact_table F, data_mart.reported_date_dimension D, data_mart.phu_location_dimension L 
+where F.reported_date_key=D.reported_date_key and F.phu_location_key=L.phu_location_key and F.is_fatal=true and D.month in ('November','December') and L.city in ('Mississauga','Ottawa') 
 GROUP BY (L.phu_name,D.month);
 
  --number of unresolved cases for rainy and snowy days in Peel and York
